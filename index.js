d76cd9ef0d23782167383a9640a776ccd6b9f854
Aarons <spelled.aaron@gmail.com>
2022-03-18 21:13:26 -0700

    coolio update

--- index.js
+++ index.js
@@ -22,45 +22,65 @@ client.once('ready', () => {
 })
 
 client.on('messageCreate', message => {
-    if(message.author.bot) return;
-    queuing(message.author.tag, message.channel, message.content) //Function controlls queue, 
-    connect4(message.channel, message.channel.content);
+    if(message.author.bot)return;
+    queuing(message.author.tag, message.channel, message.content, message) //Function controlls queue, 
+    connect4(message.channel, message.channel.content, message);
 })
 
 
-function connect4(Channel, Content){
+function connect4(Channel, Content, Message){
     if(gameStart === true){
-        Channel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`);
         gameStart = undefined;
+        Channel.send(`Queue is full! Game is starting...`)
+        .then(Message => {
+            setTimeout(() => Message.edit(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`), 1000)
+          })
     }
 }
 
 
-function queuing(authorTag, Channel, Content)//message.author.tag, message.channel, message.content
+function queuing(authorTag, Channel, Content, Message)//message.author.tag, message.channel, message.content, message
 {
     if(queue[0] === undefined && Content === "-queue" ){
         queue[0] = authorTag
+        Message.delete();
         Channel.send(`${authorTag} has queued! 1/2`)
+        .then(Message => {
+            setTimeout(() => Message.delete(), 5000)
+          })
         console.log(`${queue[0]} has queued 1/2`)
     } else if(queue[0] != undefined && queue[1] === undefined && authorTag != queue[0] && Content === "-queue"){
         queue[1] = authorTag
+        Message.delete();
         Channel.send(`${authorTag} has queued! 2/2`)
+        .then(Message => {
+            setTimeout(() => Message.delete(), 5000)
+          })
         console.log(`${queue[0]} has queued 2/2`)
     }
     if(authorTag === queue[0] && Content === "-dequeue"){
         queue[0] = queue[1];
         queue[1] = undefined;
         Channel.send("You have dequeued successfully!")
+        .then(Message => {
+            setTimeout(() => Message.delete(), 5000)
+          })
     } else if(authorTag === queue[1] && Content === "-dequeue"){
         queue[1] = undefined;
         Channel.send("You have dequeued successfully!")
+        .then(Message => {
+            setTimeout(() => Message.delete(), 5000)
+          })
     }
 
     if(queue[0] != undefined && queue[1] != undefined && gameStart === false){
-        Channel.send(`Queue is full! Game is starting...`)
+    Message.react(`6️⃣`);
+        // })
         gameStart = true
     }
     console.table(queue)
 }
+
+
 
 client.login(token);
