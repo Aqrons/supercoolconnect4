18a3acbedb9d851eb0bdffe671999b44900916ca
Aarons <spelled.aaron@gmail.com>
2022-03-31 17:03:09 +0000

    Finished Game??????

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var turnRY = "Red"; //Current user's turn (Red or Yellow)
 var circle = undefined;//Current the color of the circle
 var pRed = undefined;// Player Red
 var pYellow = undefined;//Player Yellow
-
-var board = [ ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
-              ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
-              ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
-              ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
-              ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
-              ["|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>", "|<:blank:954191658403127307>","|"],
+var empty = "|<:blank:954191658403127307>"
+var board = [ [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
               [" 1️⃣","  2️⃣"," 3️⃣"," 4️⃣"," 5️⃣", " 6️⃣"]];
-var empty = 
+
 
 client.once('ready', () => {
     console.log(`Online!`)
@@ -49,13 +49,10 @@ function queuing(authorTag, Channel, Content, Message)//message.author.tag, mess
         queue[0] = authorTag
         Message.delete();
         Channel.send(`${authorTag} has queued! 1/2`)
-
-        console.log(`${queue[0]} has queued 1/2`)
     } else if(queue[0] != undefined && queue[1] === undefined && authorTag != queue[0] && (Content === "-queue" || Content === "-q")){
         queue[1] = authorTag
         Message.delete();
         Channel.send(`${authorTag} has queued! 2/2`)
-        console.log(`${queue[0]} has queued 2/2`)
     }
     if(authorTag === queue[0] && Content === "-dequeue"){
         queue[0] = queue[1];
@@ -67,11 +64,9 @@ function queuing(authorTag, Channel, Content, Message)//message.author.tag, mess
     }
     if(queue[0] != undefined && queue[1] != undefined && gameStart === false){
         gameStart = true
-        console.log("Game starting!")
         pRed = queue[0]
         pYellow = queue[1]
     }
-    console.table(queue)
 }
 
 function runGame(msgContent,msgChannel,msgAuthor){
@@ -91,7 +86,7 @@ function runGame(msgContent,msgChannel,msgAuthor){
         }
         if(msgContent === "1"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][0] === "|<:blank:954191658403127307>" ){
+                if(board[i][0] === empty ){
                     board[i][0] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
@@ -99,7 +94,7 @@ function runGame(msgContent,msgChannel,msgAuthor){
             }
         }else if(msgContent === "2"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][1] === "|<:blank:954191658403127307>" ){
+                if(board[i][1] === empty ){
                     board[i][1] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
@@ -107,7 +102,7 @@ function runGame(msgContent,msgChannel,msgAuthor){
             }
         }else if(msgContent === "3"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][2] === "|<:blank:954191658403127307>" ){
+                if(board[i][2] === empty ){
                     board[i][2] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
@@ -115,7 +110,7 @@ function runGame(msgContent,msgChannel,msgAuthor){
             }
         }else if(msgContent === "4"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][3] === "|<:blank:954191658403127307>" ){
+                if(board[i][3] === empty ){
                     board[i][3] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
@@ -123,7 +118,7 @@ function runGame(msgContent,msgChannel,msgAuthor){
             }
         }else if(msgContent === "5"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][4] === "|<:blank:954191658403127307>" ){
+                if(board[i][4] === empty ){
                     board[i][4] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
@@ -131,14 +126,13 @@ function runGame(msgContent,msgChannel,msgAuthor){
             }
         }else if(msgContent === "6"){
             for(var i = 6; i >= 0; i--){
-                if(board[i][5] === "|<:blank:954191658403127307>" ){
+                if(board[i][5] === empty ){
                     board[i][5] = `|${color}`
                     msgChannel.send(`> ${board[0]}\n> ${board[1]}\n> ${board[2]}\n> ${board[3]}\n> ${board[4]}\n> ${board[5]}\n> ${board[6]}`)
                     break;
                 }
             }
         }
-        console.table(board)
         checkWin(msgChannel);
         if(turnRY === "Red"){
             turnRY = "Yellow"
@@ -152,35 +146,31 @@ function checkWin(msgChannel){
     for(var i = 0; i < 6; i++){
         //checks for horizontal win
         if((board[i][5] === `|${color}` && board[i][4] === `|${color}` && board[i][3] === `|${color}` && board[i][2] === `|${color}`) || (board[i][4] === `|${color}` && board[i][3] === `|${color}` && board[i][2] ===`|${color}` && board[i][1] === `|${color}`) || (board[i][3] === `|${color}` && board[i][2] === `|${color}` && board[i][1] === `|${color}` && board[i][0] === `|${color}`)){
-            console.log("MAJOR BAG ALERT")
-            msgChannel.send(`${color} Wins`)
+            msgChannel.send(`${color} wins!`)
             gameOver();
             break;
         }
         //check for vertical win
         if((board[5][i] === `|${color}` && board[4][i] === `|${color}` && board[3][i] === `|${color}` && board[2][i] === `|${color}`) || (board[4][i] === `|${color}` && board[3][i] === `|${color}` && board[2][i] === `|${color}` && board[1][i] === `|${color}`) || (board[3][i] === `|${color}` && board[2][i] === `|${color}` && board[1][i] === `|${color}` && board[0][i] === `|${color}`)){
-            console.log("MAJOR BAG ALERT 2")
-            msgChannel.send(`${color} Wins`)
+            msgChannel.send(`${color} wins!`)
             gameOver();
             break;
         }
     }
     for(var i = 0; i < 3; i++){//checks for \ win
         if((board[5][i + 0] === `|${color}` && board[4][i + 1] === `|${color}` && board[3][i + 2] === `|${color}` && board[2][i + 3] === `|${color}`) || (board[4][i + 0] === `|${color}` && board[3][i + 1] === `|${color}` && board[2][i + 2] === `|${color}` && board[1][i + 3] === `|${color}`) || (board[3][i + 0] === `|${color}` && board[2][i + 1] === `|${color}` && board[1][i + 2] === `|${color}` && board[0][i + 3] === `|${color}`)){
-            console.log("MAJOR BAG ALRT 3")
-            msgChannel.send(`${color} Wins`)
+            msgChannel.send(`${color} wins!`)
             gameOver();
             break;
         }
         if((board[5][i + 3] === `|${color}` && board[4][i + 2] === `|${color}` && board[3][i + 1] === `|${color}` && board[2][i + 0] === `|${color}`) || (board[4][i + 3] === `|${color}` && board[3][i + 2] === `|${color}` && board[2][i + 1] === `|${color}` && board[1][i + 0] === `|${color}`) || (board[3][i + 3] === `|${color}` && board[2][i + 2] === `|${color}` && board[1][i + 1] === `|${color}` && board[0][i + 0] === `|${color}`)){
-            console.log("MAJOR BAG ALRT 4")
-            msgChannel.send(`${color} Wins`)
+            msgChannel.send(`${color} wins!`)
             gameOver();
             break;
         }
     }
-    if((board[0][0] != pRed && pYellow) && (board[0][1] != pRed && pYellow) && (board[0][2] != pRed && pYellow) && (board[0][3] != pRed && pYellow) && (board[0][4] != pRed && pYellow) && (board[0][5] != pRed && pYellow) && (board[1][0] != pRed && pYellow) && (board[1][1] != pRed && pYellow) && (board[1][2] != pRed && pYellow) && (board[1][3] != pRed && pYellow) && (board[1][4] != pRed && pYellow) && (board[1][5] != pRed && pYellow)&& (board[2][0] != pRed && pYellow)&& (board[2][1] != pRed && pYellow)&& (board[2][2] != pRed && pYellow)&& (board[2][3] != pRed && pYellow)&& (board[2][4] != pRed && pYellow)&& (board[2][5] != pRed && pYellow)&& (board[3][0] != pRed && pYellow)&& (board[3][1] != pRed && pYellow)&& (board[3][2] != pRed && pYellow)&& (board[3][3] != pRed && pYellow)&& (board[3][4] != pRed && pYellow)&& (board[3][5] != pRed && pYellow)&& (board[4][0] != pRed && pYellow)&& (board[4][1] != pRed && pYellow)&& (board[4][2] != pRed && pYellow)&& (board[4][3] != pRed && pYellow)&& (board[4][4] != pRed && pYellow) && (board[4][5] != pRed && pYellow)&& (board[5][0] != pRed && pYellow)&& (board[5][1] != pRed && pYellow)&& (board[5][2] != pRed && pYellow) && (board[5][3] != pRed && pYellow) && (board[5][4] != pRed && pYellow) && (board[5][5] != pRed && pYellow)){
-        msgChannel.send("No one wins smh")
+    if((board[0][0] != empty) && (board[0][1] != empty) && (board[0][2] != empty) && (board[0][3] != empty) && (board[0][4] != empty) && (board[0][5] != empty) && (board[1][0] != empty) && (board[1][1] != empty) && (board[1][2] != empty) && (board[1][3] != empty) && (board[1][4] != empty) && (board[1][5] != empty)&& (board[2][0] != empty)&& (board[2][1] != empty)&& (board[2][2] != empty)&& (board[2][3] != empty)&& (board[2][4] != empty)&& (board[2][5] != empty)&& (board[3][0] != empty)&& (board[3][1] != empty)&& (board[3][2] != empty)&& (board[3][3] != empty)&& (board[3][4] != empty)&& (board[3][5] != empty)&& (board[4][0] != empty)&& (board[4][1] != empty)&& (board[4][2] != empty)&& (board[4][3] != empty)&& (board[4][4] != empty) && (board[4][5] != empty)&& (board[5][0] != empty)&& (board[5][1] != empty)&& (board[5][2] != empty) && (board[5][3] !=empty) && (board[5][4] != empty) && (board[5][5] != empty)){
+        msgChannel.send("Draw, Nobody wins!")
     }
 }
 
@@ -192,6 +182,12 @@ function gameOver(){
     circle = undefined;//Current the color of the circle
     pRed = undefined;// Player Red
     pYellow = undefined;//Player Yellow
-    console.log("Game has been reset!")
+    board = [ [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [empty, empty, empty, empty, empty, empty,"|"],
+              [" 1️⃣","  2️⃣"," 3️⃣"," 4️⃣"," 5️⃣", " 6️⃣"]];
 }
 client.login(token);
