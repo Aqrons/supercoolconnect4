ef1b35cc54c731e531424102a01495b7c04ca06d
Koishe <100549481+Koishe@users.noreply.github.com>
2022-03-31 09:56:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ function checkWin(msgChannel){
 
 function drawGame(msgChannel){
     if((board[0][0] != pRed && pYellow) && (board[0][1] != pRed && pYellow) && (board[0][2] != pRed && pYellow) && (board[0][3] != pRed && pYellow) && (board[0][4] != pRed && pYellow) && (board[0][5] != pRed && pYellow) && (board[1][0] != pRed && pYellow) && (board[1][1] != pRed && pYellow) && (board[1][2] != pRed && pYellow) && (board[1][3] != pRed && pYellow) && (board[1][4] != pRed && pYellow) && (board[1][5] != pRed && pYellow)&& (board[2][0] != pRed && pYellow)&& (board[2][1] != pRed && pYellow)&& (board[2][2] != pRed && pYellow)&& (board[2][3] != pRed && pYellow)&& (board[2][4] != pRed && pYellow)&& (board[2][5] != pRed && pYellow)&& (board[3][0] != pRed && pYellow)&& (board[3][1] != pRed && pYellow)&& (board[3][2] != pRed && pYellow)&& (board[3][3] != pRed && pYellow)&& (board[3][4] != pRed && pYellow)&& (board[3][5] != pRed && pYellow)&& (board[4][0] != pRed && pYellow)&& (board[4][1] != pRed && pYellow)&& (board[4][2] != pRed && pYellow)&& (board[4][3] != pRed && pYellow)&& (board[4][4] != pRed && pYellow) && (board[4][5] != pRed && pYellow)&& (board[5][0] != pRed && pYellow)&& (board[5][1] != pRed && pYellow)&& (board[5][2] != pRed && pYellow) && (board[5][3] != pRed && pYellow) && (board[5][4] != pRed && pYellow) && (board[5][5] != pRed && pYellow)){
-        msgChannel.send("No one wins smh")
+        msgChannel.send("Draw")
     }
 }
 
